68ae9af457f9507c9c1f7373f95076e68322d969
THE_BA_MACHINE\Ben <bcalbert777@gmail.com>
2017-10-23 07:20:36 -0400

    Created new Deck Builder scene to allow player to build deck

---
+++ Assets/CardInspector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardInspector : MonoBehaviour {
+
+    // Use this for initialization
+    Dropdown dropdown;
+    public GameObject CardFieldObjects;
+    public GameObject CardFieldSpells;
+
+	void Start () {
+        dropdown = GetComponent<Dropdown>();
+        CardFieldObjects.SetActive(false);
+        CardFieldSpells.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       if (dropdown.value == 0)
+        {
+            CardFieldObjects.SetActive(true);
+            CardFieldSpells.SetActive(false);
+        }
+       else if (dropdown.value == 1)
+        {
+            CardFieldObjects.SetActive(false);
+            CardFieldSpells.SetActive(true);
+        }
+
+    }
+}
