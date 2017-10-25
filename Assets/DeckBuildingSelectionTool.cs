f69e9d4b5ec8b1e2ad6f0186d570d8e32333b1f5
THE_BA_MACHINE\Ben <bcalbert777@gmail.com>
2017-10-25 07:00:11 -0400

    Can drage Cards over to deck to add and can remove them with button. (Need to add via plus button at card, Limit amount add from Amount of cards available, Show total Deck Power)

---
+++ Assets/DeckBuildingSelectionTool.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeckBuildingSelectionTool : MonoBehaviour {
+
+    // Use this for initialization
+    Image CardImageHolding;
+
+    DeckHolder deckHolder;
+    public Transform ParentTransform;
+
+    Card[] Cards;
+    Card SelectedCard;
+    GameObject SelectedCardObject;
+
+	void Start () {
+        deckHolder = FindObjectOfType<DeckHolder>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("pointer1"))
+        {
+
+            Cards = FindObjectsOfType<Card>();
+            foreach (Card card in Cards)
+            {
+                if (card.CheckMousePositionOnButton())
+                {
+                    SelectedCard = card;
+                    SelectedCardObject = Instantiate(SelectedCard.gameObject, Input.mousePosition, Quaternion.identity);
+                    SelectedCardObject.transform.parent = ParentTransform;
+                }
+            }
+        }
+        else if (Input.GetButtonUp("pointer1"))
+        {
+            if (SelectedCard != null)
+            {
+                Debug.Log(deckHolder.CheckMousePositionOnButton());
+                if (deckHolder.CheckMousePositionOnButton())
+                {
+                    deckHolder.AddCard(SelectedCard);
+                }
+
+                Destroy(SelectedCardObject);
+                SelectedCard = null;
+                SelectedCardObject = null;
+            }
+        }
+
+        if (SelectedCard != null)
+        {
+            SelectedCardObject.transform.position = Input.mousePosition;
+        }
+
+    }
+
+}
---
+++ Assets/DeckHolder.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DeckHolder : MonoBehaviour {
+
+    // Use this for initialization
+
+    int Index = 0;
+    public List<Card> DeckCards;
+    GraphicRaycaster myGraphicsRaycaster;
+
+    public CardPosition[] CardPositionArray;
+
+
+    private void Start()
+    {
+
+        myGraphicsRaycaster = FindObjectOfType<GraphicRaycaster>();
+        foreach (CardPosition CP in CardPositionArray)
+        {
+            CP.GetComponent<Text>().text = "";
+        }
+    }
+
+    public bool CheckMousePositionOnButton()
+    {
+        PointerEventData ped = new PointerEventData(null);
+        ped.position = Input.mousePosition;
+        List<RaycastResult> results = new List<RaycastResult>();
+        myGraphicsRaycaster.Raycast(ped, results);
+       
+       foreach (RaycastResult RR in results)
+        {
+            if (RR.gameObject.GetComponent<DeckHolder>())
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void Update()
+    {
+        Debug.DrawLine(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z - 10), new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z + 10));
+    }
+
+    public void AddCard(Card card)
+    {
+        DeckCards.Add(card);
+        CardPositionArray[Index].GetComponent<Text>().text = card.name;
+        Index++;
+    }
+
+    public void RemoveCard(int indexR)
+    {
+        Debug.Log(DeckCards.Count);
+        Debug.Log(indexR);
+        if (DeckCards.Count > indexR)
+        {
+            DeckCards.Remove(DeckCards[indexR]);
+            //    CardPositionArray[Index].GetComponent<Text>().text = "";
+            ResetCards();
+                Index--;
+        }
+    }
+
+    void ResetCards()
+    {
+        for (int i = 0; i < DeckCards.Count; i++)
+        {
+            CardPositionArray[i].GetComponent<Text>().text = DeckCards[i].name;
+        }
+        CardPositionArray[DeckCards.Count].GetComponent<Text>().text = "";
+
+        //if (indexR < DeckCards.Count - 1)
+        //{
+        //    for (int i = indexR; i < DeckCards.Count - 1; i++)
+        //    {
+        //      //  CardPositionArray[indexR].GetComponent<Text>().text = DeckCards[]
+        //    }
+        //}
+    }
+
+}
